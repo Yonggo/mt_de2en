1f26fadd6cfcaaefefdc29d060d0a7369237e404
Yong.Go <yongryul.go@gmail.com>
2023-12-26 13:02:53 +0100

    changing bleu_test.py

--- bleu_test.py
+++ bleu_test.py
@@ -4,12 +4,15 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
+reference_text_file = "data/test/example.europarl.en.test"
+to_be_translated_file = "data/test/example.europarl.de.test"
+model_path = "models/model-30-0.9031.keras"
 
 if __name__ == '__main__':
-    with open("data/test/example.europarl.en.test", "r", encoding="utf8") as file:
+    with open(reference_text_file, "r", encoding="utf8") as file:
         sentences_en = file.readlines()
     references = [[[words for words in sentence.split()]] for sentence in sentences_en]
-    predictions = translate("data/test/example.europarl.de.test", "models/model-30-0.9031.keras")
+    predictions = translate(to_be_translated_file, model_path)
     #predictions = [[words for words in sentence.split()] for sentence in sentences_en]
     if len(predictions) != len(references):
         raise Exception("Size of predictions is different from size of references")
